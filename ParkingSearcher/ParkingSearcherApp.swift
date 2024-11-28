8e29ce341a4cf65cace3bd2453a50f957b9330c8
baimisaac <ibrahimisaaceg@gmail.com>
2024-11-28 12:47:12 +0800

    Initial Commit

---
+++ ParkingSearcher/ParkingSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingSearcherApp.swift
+//  ParkingSearcher
+//
+//  Created by STDC_13 on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ParkingSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
